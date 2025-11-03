5f1573ed2e40f37cf31abea04005a68e301dd4b4
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-03 11:55:41 -0300

    feat(infrastructure): create DatabaseWriteException exception for database write errors

---
+++ Infrastructure/Exceptions/DatabaseWriteException.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure.Exceptions
+{
+    public class DatabaseWriteException : InfrastructureLayerException
+    {
+        public override string Title => "Database write error";
+
+        public DatabaseWriteException() { }
+        public DatabaseWriteException(string? message) : base(message) { }
+        public DatabaseWriteException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
