b819fefea1d9c9466866039357cd4f9e845dbcf7
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-11-03 12:57:07 -0300

    feat(infrastructure): configurar DI para MongoDbContext, GenericRepository e RepositoryExecutor

---
+++ Infrastructure/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces.Repositories;
+using Infrastructure.Common;
+using Infrastructure.Data.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Infrastructure.Repositories;
+
+namespace Infrastructure
+{
+    public static class Bootstrapper
+    {
+        public static void AddInfrastructure(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddSingleton<IMongoDbContext, MongoDbContext>();
+            service.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+            service.AddScoped<IRepositoryExecutor, RepositoryExecutor>();
+        }
+    }
+}
