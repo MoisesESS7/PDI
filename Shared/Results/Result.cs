46ab8d7e3c7e168bacdae29bebb0c612bb973a29
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-12-19 16:35:23 -0300

    feat(shared): add Result class to manage success and fails in the result pattern

---
+++ Shared/Results/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace Shared.Results
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public Error? Error { get; }
+
+        public Result(bool isSuccess, Error? error)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Ok() => new (true, null);
+        public static Result Fail(Error error) => new (false, error);
+    }
+}
