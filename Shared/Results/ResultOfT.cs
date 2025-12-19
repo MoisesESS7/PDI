a50685e8596e9273551173a227810de4312a0a93
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-12-19 16:37:49 -0300

    feat(shared): add ResultOfT class to manage success and fails generic in the result pattern

---
+++ Shared/Results/ResultOfT.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.Results
+{
+    public sealed class ResultOfT<T> : Result
+    {
+        public T? Value { get; }
+
+        private ResultOfT(bool isSuccess, T? value, Error? error) : base (isSuccess, error)
+        {
+            Value = value;
+        }
+
+        public static ResultOfT<T> Ok(T value) => new (true, value, null);
+        public static new ResultOfT<T> Fail(Error error) => new (false, default, error);
+    }
+}
