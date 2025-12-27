ae049c4222bcfcb7a8cdf2c8d3e6883c986af766
Moisés Silva <Moises@12mvcn.onmicrosoft.com>
2025-12-27 12:36:38 -0300

    refactor(api): add Audit properties in the PersonResponse class

--- Api/Responses/Persons/PersonResponse.cs
+++ Api/Responses/Persons/PersonResponse.cs
@@ -6,25 +6,34 @@ namespace Api.Responses.Persons
     {
         public string Id { get; private set; }
         public string Name { get; private set; }
+        public DateTimeOffset CreatedAt { get; private set; }
+        public DateTimeOffset? UpdatedAt { get; private set; }
+        public DateTimeOffset? DeletedAt { get; private set; }
         public int Age { get; private set; }
-        public DateTime BirthDate { get; private set; }
+        public DateTimeOffset BirthDate { get; private set; }
         public string CpfNumber { get; private set; }
-        public DateTime CpfRegistrationDate { get; private set; }
+        public DateTimeOffset CpfRegistrationDate { get; private set; }
         public string RgNumber { get; private set; }
         public InssuingAuthority RgIssuingAuthority { get; private set; }
 
         public PersonResponse(
             string id,
             string name,
+            DateTimeOffset createdAt,
+            DateTimeOffset? updatedAt,
+            DateTimeOffset? deletedAt,
             int age,
-            DateTime birthDate,
+            DateTimeOffset birthDate,
             string cpfNumber,
-            DateTime cpfRegistrationDate,
+            DateTimeOffset cpfRegistrationDate,
             string rgNumber,
             InssuingAuthority rgIssuingAuthority)
         {
             Id = id;
             Name = name;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            DeletedAt = deletedAt;
             Age = age;
             BirthDate = birthDate;
             CpfNumber = cpfNumber;
